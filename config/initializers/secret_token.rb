a7a7f6c348416f25aa982c90ca33c335c98274d7
shahin70000 <shahin70000@gmail.com>
2013-05-26 12:25:26 +0300

    $ rails new zirpirani

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zirpirani::Application.config.secret_token = '5163de36559cc44030f115e2d812081f3f4d769b12a19a08d29079081ba33158746bdf020b1a87abc447586157d40289893a2d3879c15e583af9f402d3ca10e2'
