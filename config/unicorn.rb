26c889de28203e08c481150ab8e03eb77d401ddb
shahin70000 <shahin70000@gmail.com>
2013-06-02 16:57:14 +0300

    some temp fixes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,17 +1,8 @@
 root = "/home/deployer/apps/zirpirani/current"
-
 working_directory root
-
 pid "#{root}/tmp/pids/unicorn.pid"
-
 stderr_path "#{root}/log/unicorn.log"
-
 stdout_path "#{root}/log/unicorn.log"
-
-
-
 listen "/tmp/unicorn.zirpirani.sock"
-
 worker_processes 2
-
 timeout 30
