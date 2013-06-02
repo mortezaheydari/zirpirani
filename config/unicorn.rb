29c0523abf5e57881bf364240c25b16cb8a91876
shahin <shahin70000@gmail.com>
2013-06-02 12:28:13 +0300

    deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,17 @@
+root = "/home/deployer/apps/zirpirani/current"
+
+working_directory root
+
+pid "#{root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{root}/log/unicorn.log"
+
+stdout_path "#{root}/log/unicorn.log"
+
+
+
+listen "/tmp/unicorn.zirpirani.sock"
+
+worker_processes 2
+
+timeout 30
