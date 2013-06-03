285e0c705032844fcf33a8da9bb7ca1462ab4e47
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-03 12:12:35 +0300

    echo to run

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      echo "chmod a+x /etc/init.d/unicorn_#{application}"
+      run "chmod a+x /etc/init.d/unicorn_#{application}"
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
