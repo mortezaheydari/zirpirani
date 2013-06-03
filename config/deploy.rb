557af7dc7cb97114bfb0436d8fb13e33cafee374
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-03 12:19:35 +0300

    change back

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,6 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "chmod a+x /etc/init.d/unicorn_#{application}"
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
