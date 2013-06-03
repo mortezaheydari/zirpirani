e71bf8c136e0fd9443f36c7fbcd26fd84c732830
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-03 12:06:54 +0300

    chmod

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
+      echo "chmod a+x /etc/init.d/unicorn_#{application}"
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
@@ -36,7 +37,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
- 
+
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
