50cbaddb9b381e60c0dd611ea555c8c971d52300
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-03 18:20:17 +0300

    commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,8 @@ Devise.setup do |config|
   # Rails – Twitter and Facebook Authentications with Omniauth and Devise
   # Source: http://www.orhancanceylan.com/rails-twitter-and-facebook-authentications-with-omniauth-and-devise/
   config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
-  config.omniauth :facebook, "632971256717717", "8a41616c1e531575b6b733ef65f568b4"
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], {:scope => 'publish_stream, email'}
+  #
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
