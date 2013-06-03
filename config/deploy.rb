885cfa819e7e8bd576edeb0035c06453f7e99abe
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-03 12:52:39 +0300

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
