a2b58e7c6442f49bddbdc97074b2a90fc3ed582c
shahin70000 <shahin70000@gmail.com>
2013-06-10 11:32:13 +0300

    Revert "Recaptcha"
    This reverts commit 4a2c49b38f67829c38ba3f40835cff44d50dd2d4.

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'net/http'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -59,7 +58,5 @@ module Zirpirani
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-
   end
 end
--- config/initializers/recaptcha.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LfjWuISAAAAACdwmM5QIT7uAdsF4u0VKWWBCsJI'
-  config.private_key = '6LfjWuISAAAAAF437k-6MaMrCRO9q0KZXev9FmY5'
-  config.proxy = 'http://myproxy.com.au:8080'
-end
