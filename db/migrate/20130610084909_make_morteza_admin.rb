de70c5120e9ee15b523dc97f283b67d0680e396e
shahin70000 <shahin70000@gmail.com>
2013-06-10 12:15:29 +0300

    make morteza admin

---
+++ db/migrate/20130610084909_make_morteza_admin.rb
@@ -0,0 +1,13 @@
+class MakeMortezaAdmin < ActiveRecord::Migration
+  def up
+  	account = Account.find_by_email("mt_q8_2007@gmail.com")
+  	account.user.add_role :admin unless account.nil?
+  	account.user.add_role :super_admin unless account.nil?  	
+  end
+
+  def down
+  	account = Account.find_by_email("mt_q8_2007@gmail.com")  	
+  	account.user.remove_role :admin unless account.nil?
+  	account.user.remove_role :super_admin unless account.nil?  	  	
+  end
+end
