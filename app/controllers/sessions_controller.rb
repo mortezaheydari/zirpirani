d01931a3ccbfce41a73d3ae90b0eec81615d7e74
Morteza Heydari <mortezaheydari1988@gmail.com>
2013-06-11 08:47:43 +0300

    fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < Devise::SessionsController
-
+    before_filter :blocked_users_sign_in, :only => [ :create ]
   # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
+    # self.resource = warden.authenticate!(auth_options)
 
-    if resource.user.has_role? :blocked
-		redirect_to new_account_session_path, alert: "your account is suspended."
+    if self.resource.user.has_role? :blocked
+		redirect_to new_account_registration_path, alert: "your account is suspended."
     else
 	    set_flash_message(:notice, :signed_in) if is_navigational_format?
 	    sign_in(resource_name, resource)
